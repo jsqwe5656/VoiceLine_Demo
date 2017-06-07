c3fad3bb5c6d499d57425045599051f3e64edfab
zbf <516845590@qq.com>
2017-06-07 18:01:54 +0800

    添加文件读取

---
+++ app/src/main/java/zbf/voiceline_demo/PlayActivity.java
@@ -0,0 +1,19 @@
+package zbf.voiceline_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 播放声音界面
+ */
+public class PlayActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+
+    }
+}
